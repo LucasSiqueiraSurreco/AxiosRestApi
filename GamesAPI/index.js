d6f62cf30511e0ff680eeb091bcf8915908f5581
LucasSiqueiraSurreco <lucassiqueirasurreco@outlook.com>
2023-10-25 15:06:47 -0300

    axios integration

---
+++ GamesAPI/index.js
@@ -0,0 +1,66 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+const DB = {
+  games: [
+    { id: 1, title: "BF5", year: 2020, price: 120 },
+    { id: 2, title: "EFT", year: 2016, price: 270 },
+    { id: 3, title: "ARK", year: 2015, price: 450 },
+  ],
+};
+
+app.get("/games", (req, res) => {
+  res.status(200).send(DB.games);
+});
+
+app.get("/games/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (isNaN(id)) {
+    res.status(400).send("Não é um número de id");
+  } else {
+    const games = DB.games.find((game) => game.id === id);
+    if (games) {
+      res.status(201).send(games);
+    } else {
+      res.status(404).send("Game not found");
+    }
+  }
+});
+
+app.post("/game", (req, res) => {
+  const { title, price, year } = req.body;
+  const newGame = {
+    id: DB.games.length + 1,
+    title,
+    price,
+    year,
+  };
+
+  DB.games.push(newGame);
+  res.status(201).send(newGame);
+});
+
+app.delete("/game/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (isNaN(id)) {
+    res.status(400).send("Não é um número de id");
+  } else {
+    const gameIndex = DB.games.findIndex((game) => game.id === id);
+    if (gameIndex !== -1) {
+      DB.games.splice(gameIndex, 1);
+      res.status(200).send("Deletado");
+    } else {
+      res.status(404).send("Game not found");
+    }
+  }
+});
+
+app.put("/game/:id", (req, res) => {});
+
+app.listen(3333, () => console.log("http://localhost:3333"));
